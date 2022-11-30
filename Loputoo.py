05194ae33f534d268965c459b141fe44fd1bf558
Josten <josten11@hotmail.com>
2022-11-30 21:26:49 +0200

    Ekraan

--- Loputoo.py
+++ Loputoo.py
@@ -1,16 +1,27 @@
 import pygame 
 import random
 
-
+Ussi_kiirus=15
 Ekraani_K6rgus= 720
 Ekraani_Laius= 480
 
-#
+fps=pygame.time.Clock()
+
 pygame.init
 
 pygame.display.set_caption("V6imas Ussim2ng!")
+while True:
+    Ekraan = pygame.display.set_mode((Ekraani_K6rgus,Ekraani_Laius))
+    
 
-Ekraan = pygame.display.set_mode((Ekraani_K6rgus,Ekraani_Laius))
-fps=pygame.time.Clock()
 
 
+
+    
+    for event in pygame.event.get():
+        #Et saaks programmi x kaudu kinni panna
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+            
+            
